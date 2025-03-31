1a3b8ae5e8e1eb0eb726358319d764729db68536
Aditya0545 <adityakumar2019.ak@gmail.com>
2025-03-31 07:43:57 +0530

    fixed

--- next.config.js
+++ next.config.js
@@ -4,14 +4,13 @@ const nextConfig = {
   swcMinify: true,
   output: 'standalone',
   images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'drive.google.com',
-      'docs.google.com',
-      'img.youtube.com',
-      'i.ytimg.com',
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
-    formats: ['image/avif', 'image/webp'],
   },
   // Enable trailing slashes for Netlify
   trailingSlash: true,
@@ -37,6 +36,18 @@ const nextConfig = {
         ],
       },
     ];
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+      };
+    }
+    return config;
   },
 };
 
